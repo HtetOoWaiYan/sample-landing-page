1addbd571a026cf60a642393ea8e3e2f87e1d32f
Htet Oo Wai Yan <htetoo.waiyan@outlook.com>
2022-06-09 14:56:58 +0630

    refactor: add eslintrc

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { Button } from 'antd'; 
-import './App.css';
+import React from 'react'
+import { Button } from 'antd'
+import './App.css'
 
 const App = () => (
-  <div className="App">
-    <Button type="primary">Button</Button>
-  </div>
+	<div className='App'>
+		<Button type='primary'>Button</Button>
+	</div>
 )
 
-export default App;
+export default App
